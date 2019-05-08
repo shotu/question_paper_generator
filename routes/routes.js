deb377344003c09bc3a4b539d01341c768cff542
Manish <manish@koinok.com>
2019-05-08 16:44:44 +0530

    Minor update

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,12 @@ var appRouter = function (app) {
   app.get("/api/questions", function(req, res) {    
     let query = req.query
     return questionsController.getQuestions(query)
-    .then(res_data =>{
-
+    .then(res_data => {
       res.json({"Questions": res_data});
-    }).catch(e=>{
-      console.log("dadada errrer", e)
+    }).catch(e=> {
+      console.log("Application error", e);
       res.status(500).send("Some thing went wrong");
-    })    
+    })
   });
 }
 module.exports = appRouter;
